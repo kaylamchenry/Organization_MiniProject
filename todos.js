29b4145fa0fe64c8cc9583622218fa6d8999bb39
kaylamchenry <kayla.mchenry@gmail.com>
2023-12-06 13:34:19 -0800

    APIFetchrequest

--- todos.js
+++ todos.js
@@ -5,22 +5,33 @@ window.onload = init();
 function getUserTasks() {
     // Get the selected user's ID from the dropdown
     let selectedUserId = document.getElementById("userDropdown").value;
+    const baseURL = "http://localhost:8083/";
+    const endpointResource = "api/todos/byuser/";
 
-//label the variables
-function fetchData(_endpointResource) {
-    const baseURL = "https://api.weather.gov/";
-    const endpointResource = "weather.gov";
-    const searchValue = "";
-    const api_key = "";
 
-//fetch api data 
-    fetch(baseURL + endpointResource).then((response) =>
-      response.json().then((data) => displayData(data))
-    );
+    function fetchData(endpointResource) {
+      const baseURL = "http://localhost:8083/api/todos/byuser/";
+      fetch(baseURL + endpointResource)
+          .then(response => response.json())
+          .then(data => displayData(data))
+    }
+  
+    function displayData(data) {
+      let todoTasksDiv = document.getElementById("todoTasks");
+      todoTasksDiv.innerHTML = "<h2>ToDo Tasks</h2>";
+      
+      if (data.length > 0) {
+          data.forEach(task => {
+              todoTasksDiv.innerHTML += `<p>${task.title}</p>`;
+          });
+      } else {
+          todoTasksDiv.innerHTML += "<p>No tasks found for the selected user.</p>";
+      }
   }
 
-//display data
-  function displayData(_data) {
-    console.log(data);
-  }
-  fetchData("weather.gov");}
+  function getUserTasks() {
+      let selectedUserId = document.getElementById("userDropdown").value;
+      fetchData(`todos/byuser/${selectedUserId}`);
+
+  
+  }}
