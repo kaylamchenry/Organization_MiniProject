bec77ff1041169dbd01955131f8251ca3f35fc7a
kaylamchenry <kayla.mchenry@gmail.com>
2023-12-06 13:30:18 -0800

    updatedIndexHref

--- todos.js
+++ todos.js
@@ -4,25 +4,23 @@ window.onload = init();
 
 function getUserTasks() {
     // Get the selected user's ID from the dropdown
-    var selectedUserId = document.getElementById("userDropdown").value;
+    let selectedUserId = document.getElementById("userDropdown").value;
 
-    // Make an AJAX request or fetch API to retrieve ToDo tasks based on the selected user's ID
-    // Replace the following placeholder code with actual logic to fetch ToDo tasks
-    // For example, you can use the fetch API as shown below
-    fetch(`/api/todotasks?userId=${selectedUserId}`)
-        .then(response => response.json())
-        .then(data => {
-            // Display ToDo tasks in the todoTasks div
-            var todoTasksDiv = document.getElementById("todoTasks");
-            todoTasksDiv.innerHTML = "<h2>ToDo Tasks</h2>";
-            
-            if (data.length > 0) {
-                data.forEach(task => {
-                    todoTasksDiv.innerHTML += `<p>${task.title}</p>`;
-                });
-            } else {
-                todoTasksDiv.innerHTML += "<p>No tasks found for the selected user.</p>";
-            }
-        })
-        .catch(error => console.error('Error fetching ToDo tasks:', error));
-}
+//label the variables
+function fetchData(_endpointResource) {
+    const baseURL = "https://api.weather.gov/";
+    const endpointResource = "weather.gov";
+    const searchValue = "";
+    const api_key = "";
+
+//fetch api data 
+    fetch(baseURL + endpointResource).then((response) =>
+      response.json().then((data) => displayData(data))
+    );
+  }
+
+//display data
+  function displayData(_data) {
+    console.log(data);
+  }
+  fetchData("weather.gov");}
