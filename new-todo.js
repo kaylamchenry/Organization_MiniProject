0a0f0b7afc65630d911597caea50a8930b671a69
kaylamchenry <kayla.mchenry@gmail.com>
2023-12-07 10:02:37 -0800

    finalsavepoint

---
+++ new-todo.js
@@ -0,0 +1,21 @@
+"use strict";
+
+window.onload = function () {
+    fetchUserNames();
+    fetchCategoryNames();
+
+//Assuming you have a function to fetch user names and category names from the API
+    function fetchUserNames() {
+        // Call API to get user names and populate the #userName dropdown
+    }
+
+    function fetchCategoryNames() {
+        // Call API to get category names and populate the #category dropdown
+    }
+
+    function submitToDo() {
+        // Implement the logic to gather form data and submit ToDo task
+        // You can use document.getElementById('elementId').value to get the values
+        // Perform any additional validations or API calls if necessary
+    }
+}
--- todos.js
+++ todos.js
@@ -1,37 +1,23 @@
 "use strict";
 
 window.onload = init();
+function init() {
+    createUserDropdown();
+}
 
-function getUserTasks() {
-    // Get the selected user's ID from the dropdown
-    let selectedUserId = document.getElementById("userDropdown").value;
-    const baseURL = "http://localhost:8083/";
-    const endpointResource = "api/todos/byuser/";
-
-
-    function fetchData(endpointResource) {
-      const baseURL = "http://localhost:8083/api/todos/byuser/";
-      fetch(baseURL + endpointResource)
-          .then(response => response.json())
-          .then(data => displayData(data))
-    }
+//Create ID, Form & Fetch Data 
+function createUserDropdown() {
+    let dropdown=document.getElementById("userDropdown");
+    const baseURL="http://localhost:8083/api/users";
+    fetch(baseURL)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(data => {
+            const userName=document.createElement("option");
+            userName.textContent= data.name;
+            userName.value= data.id;
+            dropdown.appendChild(userName);
+        })
+    })
   
-    function displayData(data) {
-      let todoTasksDiv = document.getElementById("todoTasks");
-      todoTasksDiv.innerHTML = "<h2>ToDo Tasks</h2>";
-      
-      if (data.length > 0) {
-          data.forEach(task => {
-              todoTasksDiv.innerHTML += `<p>${task.title}</p>`;
-          });
-      } else {
-          todoTasksDiv.innerHTML += "<p>No tasks found for the selected user.</p>";
-      }
-  }
-
-  function getUserTasks() {
-      let selectedUserId = document.getElementById("userDropdown").value;
-      fetchData(`todos/byuser/${selectedUserId}`);
-
-  
-  }}
+}
